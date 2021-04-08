e8ae1fcf9bd32117109a3c44f2a761216e719870
skullquake <ockert8080@gmail.com>
2021-04-08 05:27:58 -0700

    .

--- src/template/src/a.cpp
+++ src/template/src/a.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
+#include<chrono>
 #include"cpr/cpr.h"
 #include"json/nlohmann/json.hpp"
-#include<thread>
+#include"ctpl/ctpl_stl.h"
 int main(void){
+	auto tik=std::chrono::high_resolution_clock::now();
 	//curl -H "Content-Type: application/json" --data '{"foo":"bar"}' http://www.httpbin.org/post
-	auto work=[](){
+	auto work=[](int id){
+		std::cout<<"thread["<<id<<"]:start"<<std::endl;
 		nlohmann::json jreq;
 		jreq["foo"]="bar";
 		cpr::Response r=cpr::Post(
@@ -14,14 +17,14 @@ int main(void){
 			cpr::Header{{"Content-Type","application/json"}}
 		);
 		auto jres=nlohmann::json::parse(r.text);
-		std::cout<<jres.dump(4)<<std::endl;
+		//std::cout<<jres.dump(4)<<std::endl;
+		std::cout<<"thread["<<id<<"]:end"<<std::endl;
 	};
-	std::thread t0(work);
-	std::thread t1(work);
-	std::thread t2(work);
-	std::thread t3(work);
-	t0.join();
-	t1.join();
-	t2.join();
-	t3.join();
+	ctpl::thread_pool p(8);
+	for(int i=0;i<8;i++)p.push(std::ref(work));
+	p.stop(true);
+	auto tok=std::chrono::high_resolution_clock::now();
+	auto dur=(tok-tik).count();
+	//std::cout<<"Duration: "<<dur<<" us"<<std::endl;
+	std::cout<<"Duration: "<<dur/1000000<<" ms"<<std::endl;
 }
